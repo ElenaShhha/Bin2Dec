d69024003a8b2e9c7f48d7181937ea0027081b4c
Elena Shabalina <elenapolkova@yandex.ru>
2025-02-17 08:49:02 +0500

    Add files via upload

---
+++ Bin2Dec/Bin2Dec/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bin2Dec
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string valueBin = "";
+            do
+            {
+                valueBin = Input();
+            }
+            while (IsBinary(valueBin) == false || valueBin.Length >= 8);
+
+
+            int n = valueBin.Length - 1;
+            int result = 0;
+
+            foreach (char c in valueBin)
+            {
+                int charToInt = c - '0';
+                result = result + charToInt * Convert.ToInt16(Math.Pow(2, n));
+                n--;
+            }
+            System.Console.WriteLine("Число в десятичной степени - {0}", result);
+            Console.WriteLine("\nНажмите любую клавишу для выхода...");
+            Console.ReadKey();
+        }
+
+        static string Input()
+        {
+            System.Console.WriteLine("Введите число в двоичной системе счисления");
+            return System.Console.ReadLine();
+        }
+
+        static bool IsBinary(string value)
+        {
+            foreach (char c in value)
+            {
+                if (c != '0' && c != '1')
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
